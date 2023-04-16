9098210e2e5f5a8b3eb8efaf4181fcc17b11a21d
Diego Lopes <82356386+diegotopow@users.noreply.github.com>
2023-04-15 22:52:44 -0300

    Initial config Application Properties and README

---
+++ src/main/java/br/com/friendlyhost/FriendlyhostApplication.java
@@ -0,0 +1,13 @@
+package br.com.friendlyhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyhostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/friendlyhost/FriendlyhostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.friendlyhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
