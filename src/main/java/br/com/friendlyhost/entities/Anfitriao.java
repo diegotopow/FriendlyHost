5a3f449c19ceab490660f19ad393453f5cd2dd83
Diego Lopes <82356386+diegotopow@users.noreply.github.com>
2023-05-19 23:23:16 -0300

    Diego  (#4)
    * Add Anfitriao class
    
    * Add AnfitriaoService, script DB, update Anfitriao

---
+++ src/main/java/br/com/friendlyhost/entities/Anfitriao.java
@@ -0,0 +1,64 @@
+package br.com.friendlyhost.entities;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_anfitriao")
+public class Anfitriao {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idAnfitriao;
+    @ForeignKey(name = "id_quarto")// Chave estrangeira
+    private Long idQuarto;
+    @Column(length = 50, nullable = false)// Tamanho Max campo; Campo Obrigatorio;
+    private String nome;
+    @Column(length = 12, nullable = false)
+    private String cpf;
+    @Column(length = 50, nullable = false)
+    private String email;
+    @Column(length = 15, nullable = false)
+    private String telefone;
+    @Column(length = 9, nullable = false)
+    private String cep;
+    @Column(length = 30, nullable = false)
+    private String logradouro;
+    @Column(length = 10, nullable = false)
+    private int numero;
+    @Column(length = 20, nullable = false)
+    private String cidade;
+    @Column(length = 2, nullable = false)
+    private String estado;
+    @Column(length = 30, nullable = false)
+    private String nacionalidade;
+    @Column(length = 255, nullable = false)
+    private String urlCompResi;
+    @Column(length = 20, nullable = false)
+    private String bairro;
+    @Column(length = 50, nullable = false)
+    private String conjuge;
+    @Column(length = 30, nullable = false)
+    private String complemento;
+    @Column(length = 10, nullable = false)
+    private Boolean estadoCivil;
+    @Column(nullable = false)
+    private DateTimeFormat dataNascimento;
+    @Column(length = 10, nullable = false)
+    private String senha;
+}
---
+++ src/main/java/br/com/friendlyhost/services/AnfitriaoService.java
@@ -0,0 +1,22 @@
+package br.com.friendlyhost.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.friendlyhost.dto.AnfitriaoDTO;
+import br.com.friendlyhost.entities.Anfitriao;
+import br.com.friendlyhost.repository.AnfitriaoRepository;
+
+@Service
+public class AnfitriaoService {
+    
+    @Autowired
+	private AnfitriaoRepository anfitriaoRepository;
+	
+	public List<AnfitraoDTO> finAll(){
+		List<Anfitriao> result = anfitriaoRepository.findAll();
+		List<AnfitriaoDTO> dto = result.stream().map(x -> new AlunoDTO(x)).toList();
+		return dto;
+	}
+}
