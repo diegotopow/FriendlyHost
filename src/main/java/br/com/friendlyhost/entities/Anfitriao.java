c2c63ca998ca5da68156b33b67a19e1cff0684f5
diegotopow <diego_topow@hotmail.com>
2023-05-19 23:18:37 -0300

    Add AnfitriaoService, script DB, update Anfitriao

--- src/main/java/br/com/friendlyhost/entities/Anfitriao.java
+++ src/main/java/br/com/friendlyhost/entities/Anfitriao.java
@@ -27,22 +27,38 @@ public class Anfitriao {
 	private Long idAnfitriao;
     @ForeignKey(name = "id_quarto")// Chave estrangeira
     private Long idQuarto;
-    @Column(length = 50, nullable = false)// Tamanho Max campo; Campo Obrigatorio; O nome não pode se repetir.
+    @Column(length = 50, nullable = false)// Tamanho Max campo; Campo Obrigatorio;
     private String nome;
+    @Column(length = 12, nullable = false)
     private String cpf;
+    @Column(length = 50, nullable = false)
     private String email;
+    @Column(length = 15, nullable = false)
     private String telefone;
+    @Column(length = 9, nullable = false)
     private String cep;
+    @Column(length = 30, nullable = false)
     private String logradouro;
+    @Column(length = 10, nullable = false)
     private int numero;
+    @Column(length = 20, nullable = false)
     private String cidade;
+    @Column(length = 2, nullable = false)
     private String estado;
+    @Column(length = 30, nullable = false)
     private String nacionalidade;
+    @Column(length = 255, nullable = false)
     private String urlCompResi;
+    @Column(length = 20, nullable = false)
     private String bairro;
+    @Column(length = 50, nullable = false)
     private String conjuge;
+    @Column(length = 30, nullable = false)
     private String complemento;
+    @Column(length = 10, nullable = false)
     private Boolean estadoCivil;
+    @Column(nullable = false)
     private DateTimeFormat dataNascimento;
+    @Column(length = 10, nullable = false)
     private String senha;
 }
