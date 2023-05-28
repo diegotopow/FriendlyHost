4d4bba789e2431ab1d500435c0d2bffb55853417
Luziano <luziano-farias1993@hotmail.com>
2023-05-28 04:42:46 -0300

    register method

--- src/main/java/br/com/friendlyhost/controllers/AlunoController.java
+++ src/main/java/br/com/friendlyhost/controllers/AlunoController.java
@@ -3,13 +3,17 @@ package br.com.friendlyhost.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.friendlyhost.dto.AlunoDTO;
 import br.com.friendlyhost.dto.AlunoMinDTO;
+import br.com.friendlyhost.entities.Aluno;
 import br.com.friendlyhost.services.AlunoService;
 
 @RestController
@@ -18,6 +22,11 @@ public class AlunoController {
 	
 	@Autowired
 	private AlunoService alunoService;
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<?> cadastrar(@RequestBody Aluno aluno){
+		return alunoService.cadastrar(aluno);
+	}
 	
 	
 	@GetMapping(value = "/{id}")
--- src/main/java/br/com/friendlyhost/services/AlunoService.java
+++ src/main/java/br/com/friendlyhost/services/AlunoService.java
@@ -3,6 +3,8 @@ package br.com.friendlyhost.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,13 +19,19 @@ public class AlunoService {
 	@Autowired
 	private AlunoRepository alunoRepository;
 	
+
+	public ResponseEntity<?> cadastrar(Aluno aluno){
+		return new ResponseEntity<Aluno>(alunoRepository.save(aluno), HttpStatus.CREATED);
+	}
+
+	// Listar por id
 	@Transactional(readOnly = true)
 	public AlunoDTO findById(Long id){
 		Aluno result = alunoRepository.findById(id).get();
 		return new AlunoDTO(result);
-
 	}
 	
+	// Listar todos
 	@Transactional(readOnly = true)
 	public List<AlunoMinDTO> finAll(){
 		List<Aluno> result = alunoRepository.findAll();
