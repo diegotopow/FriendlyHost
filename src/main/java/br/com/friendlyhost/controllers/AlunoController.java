d975f77702ed927dccd04e4cd8b2cdcf56955cbe
Luziano <luziano-farias1993@hotmail.com>
2023-06-07 03:13:02 -0300

    Register Method

--- src/main/java/br/com/friendlyhost/controllers/AlunoController.java
+++ src/main/java/br/com/friendlyhost/controllers/AlunoController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,27 +20,38 @@ import br.com.friendlyhost.entities.Aluno;
 import br.com.friendlyhost.services.AlunoService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/alunos")
 public class AlunoController {
 	
 	@Autowired
 	private AlunoService alunoService;
 
+	@DeleteMapping("/remover/{id}")
+	public ResponseEntity<?> remover(@PathVariable long id){
+		return alunoService.remover(id);
+	}
+
+	@PutMapping("/alterar")
+	public ResponseEntity<Aluno> alterar(@RequestBody Aluno aluno){
+		return alunoService.cadastrarAlterar(aluno, "alterar");
+	}
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<?> cadastrar(@RequestBody Aluno aluno){
-		return alunoService.cadastrar(aluno);
+	public ResponseEntity<Aluno> cadastrar(@RequestBody Aluno aluno){
+		return alunoService.cadastrarAlterar(aluno, "cadastrar");
 	}
 	
 	
 	@GetMapping(value = "/{id}")
-	public AlunoDTO findDyId(@PathVariable Long id) {
-		AlunoDTO result = alunoService.findById(id);
+	public AlunoMinDTO findDyId(@PathVariable Long id) {
+		AlunoMinDTO result = alunoService.findById(id);
 		return result;
 	}
 
 	@GetMapping
-	public List<AlunoMinDTO> findAll() {
-		List<AlunoMinDTO> result = alunoService.finAll();
+	public List<AlunoDTO> findAll() {
+		List<AlunoDTO> result = alunoService.finAll();
 		return result;
 	}
 }
