a5ad57c704bf7f02e6804ece790cc4362fcbc4e7
Luziano <luziano-farias1993@hotmail.com>
2023-06-07 03:15:35 -0300

    Merge branch 'master' into Luziano

--- src/main/java/br/com/friendlyhost/entities/Anfitriao.java
+++ src/main/java/br/com/friendlyhost/entities/Anfitriao.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
