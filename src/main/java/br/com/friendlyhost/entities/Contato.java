6ba1c30dfa517703a72e609a9f0fb5173758cb6d
diegotopow <diego.topow@gmail.com>
2023-06-11 23:16:43 -0300

    Update files and methods with erros.

---
+++ src/main/java/br/com/friendlyhost/entities/Contato.java
@@ -0,0 +1,28 @@
+package br.com.friendlyhost.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_contato")
+public class Contato {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idContato;
+    @Column(length = 50, nullable = false)// Tamanho Max campo; Campo Obrigatorio;
+    private String nome;
+    @Column(length = 50, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    @DateTimeFormat
+    private LocalDate dataCadastro;
+}
